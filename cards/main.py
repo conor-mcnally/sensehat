d15b6e711d0e920943772b859033e1f312df1bf9
Conor <cmcn@gmx.co.uk>
2018-10-19 15:27:25 +0100

    --

--- cards/main.py
+++ cards/main.py
@@ -10,7 +10,7 @@ b = [0, 0, 0]
 g = [255, 0, 0]
 
 heart = [
-b,g,b,g,b,b,b,b,
+g,g,b,g,g,b,b,b,
 g,g,g,g,g,b,b,b,
 b,g,g,g,b,b,b,b,
 b,b,g,b,b,b,b,b,
@@ -20,17 +20,30 @@ b,b,b,b,b,b,b,b,
 b,b,b,b,b,b,b,b,
 ]
 
+
 sense.set_pixels(heart)
-# two = [
-# b,b,b,b,b,b,b,b,
-# b,g,g,b,b,b,b,b,
-# b,g,g,b,b,b,b,b,
-# b,b,b,b,b,b,b,b,
-# b,b,b,b,b,b,b,b,
-# b,b,b,b,g,g,b,b,
-# b,b,b,b,g,g,b,b,
-# b,b,b,b,b,b,b,b,
-# ]
+
+spade = [
+b,b,b,b,b,b,b,b,
+b,b,b,b,b,b,b,b,
+b,b,b,b,b,b,b,b,
+b,b,b,b,b,b,b,b,
+b,b,b,b,b,b,b,b,
+b,b,b,b,b,b,b,b,
+b,b,b,b,b,b,b,b,
+b,b,b,b,b,b,b,b,
+]
+
+blank = [
+b,b,b,b,b,b,b,b,
+b,b,b,b,b,b,b,b,
+b,b,b,b,b,b,b,b,
+b,b,b,b,b,b,b,b,
+b,b,b,b,b,b,b,b,
+b,b,b,b,b,b,b,b,
+b,b,b,b,b,b,b,b,
+b,b,b,b,b,b,b,b,
+]
 
 # three = [
 # g,g,b,b,b,b,b,b,
